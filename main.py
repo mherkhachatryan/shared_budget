b0a254144099a2d0bdbc795d9640e58e9ecd397a
mherkhachatryan <mherkhachatryan35912@gmail.com>
2020-11-06 11:22:42 +0400

    fixed column names

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ import warnings
 def shared_budget(filename="data.csv"):
     df = pd.read_csv(filename)
     # Doing some data processing to replace nonconsumer with consumer
-    df.rename(columns={"-Consumer": "nonConsumer"}, inplace=True)
     all_consumers = ["Նարեկ", "Վաչիկ", "Մհեր"]
     df.nonConsumer[df.nonConsumer.isna()] = ", ".join(all_consumers)
     df.nonConsumer[df.nonConsumer == "Բոլորը"] = ", ".join(all_consumers)
-    df["Buyer "] = df["Buyer "].apply(lambda x: x.strip())  # removing spaces in data
+    df["Buyer"] = df["Buyer"].apply(lambda x: x.strip())  # removing spaces in data
     df["nonConsumer"] = df["nonConsumer"].apply(lambda x: x.strip())
     for consumer in all_consumers:
         df.nonConsumer[df.nonConsumer == consumer] = ", ".join([i for i in all_consumers if i != consumer])
@@ -22,13 +21,13 @@ def shared_budget(filename="data.csv"):
         # adding 0 amount padding for consumers to avoid the not invest but consume problem
         df_consumer = df.iloc[0]
         df_consumer["Amount"] = 0
-        df_consumer["Buyer "] = consumer
+        df_consumer["Buyer"] = consumer
         df = df.append(df_consumer)
         # creating dict of consumer:overall consumption 
         consume_dict[consumer] = np.sum(df.Amount / df.consumer.apply(lambda x: len(x.split(", "))
         if consumer in x else None))
     # creating dict of consumer: averall contribution 
-    investment_dict = pd.DataFrame.to_dict(df.groupby(by="Buyer ").agg("sum"))["Amount"]
+    investment_dict = pd.DataFrame.to_dict(df.groupby(by="Buyer").agg("sum"))["Amount"]
     summary_dict = {key: investment_dict[key] - consume_dict.get(key, 0) for key in investment_dict}
     return summary_dict
 
